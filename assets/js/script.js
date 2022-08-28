b3ab30851e125d15933809a0c288693036b58d31
William Chalk <3halk1@gmail.com>
2022-08-28 14:25:26 -0400

    added date functionality along with color representation of dates

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,6 +11,8 @@ var createTask = function (taskText, taskDate, taskList) {
   // append span and p element to parent li
   taskLi.append(taskSpan, taskP);
 
+  auditTask(taskLi);
+
   // append to ul list on the page
   $("#list-" + taskList).append(taskLi);
 };
@@ -36,6 +38,20 @@ var loadTasks = function () {
       createTask(task.text, task.date, list);
     });
   });
+};
+
+var auditTask = function (taskEl) {
+  var date = $(taskEl).find("span").text().trim();
+
+  var time = moment(date, "L").set("hour", 17);
+
+  $(taskEl).removeClass("list-group-item-warning list-group-item-danger");
+
+  if (moment().isAfter(time)) {
+    $(taskEl).addClass("list-group-item-danger");
+  } else if (Math.abs(moment().diff(time, "days")) <= 2) {
+    $(taskEl).addClass("list-group-item-warning");
+  }
 };
 
 var saveTasks = function () {
@@ -121,12 +137,19 @@ $(".list-group").on("click", "span", function () {
     .val(date);
   $(this).replaceWith(dateInput);
 
+  dateInput.datepicker({
+    minDate: 1,
+    onClose: function () {
+      $(this).trigger("change");
+    },
+  });
+
   // automatically bring up the calendar
   dateInput.trigger("focus");
 });
 
 // value of due date was changed
-$(".list-group").on("blur", "input[type='text']", function () {
+$(".list-group").on("change", "input[type='text']", function () {
   var date = $(this).val();
 
   // get status type and position in the list
@@ -142,6 +165,8 @@ $(".list-group").on("blur", "input[type='text']", function () {
     .addClass("badge badge-primary badge-pill")
     .text(date);
   $(this).replaceWith(taskSpan);
+
+  auditTask($(taskSpan).closest(".list-group-item"));
 });
 
 // remove all tasks
@@ -204,6 +229,10 @@ $("#trash").droppable({
   out: function (event, ui) {
     console.log("out");
   },
+});
+
+$("#modalDueDate").datepicker({
+  minDate: 1,
 });
 
 // load tasks for the first time
