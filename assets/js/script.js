239fda2259d4b966bfd5cdabc3ddaed7c5e20ed0
William Chalk <3halk1@gmail.com>
2022-08-28 15:11:10 -0400

    added touch ups

--- assets/js/script.js
+++ assets/js/script.js
@@ -184,16 +184,18 @@ $(".card .list-group").sortable({
   tolerance: "pointer",
   helper: "clone",
   activate: function (event) {
-    console.log("activate", this);
+    $(this).addClass("dropover");
+    $(".bottom-trash").addClass("bottom-trash-drag");
   },
   deactivate: function (event) {
-    console.log("deactivate", this);
+    $(this).removeClass("dropover");
+    $(".bottom-trash").removeClass("bottom-trash-drag");
   },
   over: function (event) {
-    console.log("over", event.target);
+    $(event.target).addClass("dropover-active");
   },
   out: function (event) {
-    console.log("out", event.target);
+    $(event.target).removeClass("dropover-active");
   },
   update: function (event) {
     var tempArr = [];
@@ -234,6 +236,13 @@ $("#trash").droppable({
 $("#modalDueDate").datepicker({
   minDate: 1,
 });
+
+setInterval(function () {
+  $(".card .list-group-item").each(function (index, el) {
+    auditTask(el);
+  });
+  //runs every 30 minutes
+}, 1000 * 60 * 30);
 
 // load tasks for the first time
 loadTasks();
